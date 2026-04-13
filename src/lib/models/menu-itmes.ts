3b83e288079014d33d0d4c1b71b46814fda2977e
Ctw <ctw@ctwhome.com>
2026-04-13 07:12:20 +0200

    feat: add Documentation to public and admin navigation

--- src/lib/models/menu-itmes.ts
+++ src/lib/models/menu-itmes.ts
@@ -1,5 +1,6 @@
 export default [
 	{ title: 'Collections', path: '/collections', displayTitle: 'Collections' },
 	{ title: 'Editions', path: '/editions', displayTitle: 'Editions' },
+	{ title: 'Documentation', path: '/documentation', displayTitle: 'Documentation' },
 	{ title: 'About', path: '/about', displayTitle: 'About' }
 ];
