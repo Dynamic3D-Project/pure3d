7aff0edc422cb1314f2f2945e953c6ffb7b3f6ab
Ctw <ctw@ctwhome.com>
2026-06-23 16:52:29 +0200

    feat(LoginButton): move "My Work" menu item to a new position for improved accessibility

--- src/lib/components/ui/Login/LoginButton.svelte
+++ src/lib/components/ui/Login/LoginButton.svelte
@@ -42,29 +42,6 @@
 						<span class="text-xs text-base-content/70">{authStore.user?.email}</span>
 					</li>
 					<div class="divider my-0"></div>
-					<li>
-						<a
-							href="{base}/reviews"
-							class="flex items-center gap-2"
-							onclick={() => (accountMenuOpen = false)}
-						>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								class="size-5"
-								fill="none"
-								viewBox="0 0 24 24"
-								stroke="currentColor"
-							>
-								<path
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="2"
-									d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
-								/>
-							</svg>
-							My Work
-						</a>
-					</li>
 					{#if authStore.globalRole === GlobalRole.Admin}
 						<li>
 							<a
@@ -112,6 +89,29 @@
 								/>
 							</svg>
 							Profile
+						</a>
+					</li>
+					<li>
+						<a
+							href="{base}/reviews"
+							class="flex items-center gap-2"
+							onclick={() => (accountMenuOpen = false)}
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="size-5"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"
+								/>
+							</svg>
+							My Work
 						</a>
 					</li>
 					<div class="divider my-0"></div>
